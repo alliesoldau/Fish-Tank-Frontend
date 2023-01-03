7ec38a90fe072ff50b31a086908208d3ab1096ce
Allie Soldau <alliesoldau@gmail.com>
2023-01-03 17:41:39 -0500

    I can't get the fish details to display unless ive already selected a fish. and if i haven't already selected a fish it wont display anything

--- src/components/CritterDetails.js
+++ src/components/CritterDetails.js
@@ -4,21 +4,21 @@ function CritterDetails({ selectedCritter }) {
 
     console.log(selectedCritter.critter_name)
     console.log(selectedCritter.critter_type)
-    console.log(selectedCritter.food.food_name)
-    console.log(selectedCritter.food.food_type)
-    console.log(selectedCritter.environment.environment_name)
-    console.log(selectedCritter.environment.water_type)
-    console.log(selectedCritter.environment.water_temperature)
+    // console.log(selectedCritter.food.food_name)
+    // console.log(selectedCritter.food.food_type)
+    // console.log(selectedCritter.environment.environment_name)
+    // console.log(selectedCritter.environment.water_type)
+    // console.log(selectedCritter.environment.water_temperature)
 
     return(
         <div className="critter-details">
             <p>{selectedCritter.critter_name}</p>
             <p>{selectedCritter.critter_type}</p>
-            <p>{selectedCritter.food.food_name}</p>
+            {/* <p>{selectedCritter.food.food_name}</p>
             <p>{selectedCritter.food.food_type}</p>
             <p>{selectedCritter.environment.environment_name}</p>
             <p>{selectedCritter.environment.water_type}</p>
-            <p>{selectedCritter.environment.water_temperature}</p>
+            <p>{selectedCritter.environment.water_temperature}</p> */}
         </div> 
     )
 }
